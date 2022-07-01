31263a5602b6f3b32e7a3b1868e2fd7abdea0518
Sakura <63400019+sakura-910@users.noreply.github.com>
2022-07-02 00:34:56 +0800

    Add files via upload

---
+++ dataset/__init__.py
@@ -0,0 +1,4 @@
+from .jmnet import JMNET_IC15, JMNET_TT, JMNET_CTW, JMNET_MSRA, JMNET_Synth
+from .builder import build_data_loader
+
+__all__ = ['JMNET_IC15', 'JMNET_TT', 'JMNET_CTW', 'JMNET_MSRA', 'JMNET_Synth']
